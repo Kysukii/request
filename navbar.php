ad5c77adc97c2bba2982b5fbde65de1c6dc91f2f
Kysukii <marlon.co@hotmail.com>
2023-07-26 13:06:34 -0300

    navbar ok

--- navbar.php
+++ navbar.php
@@ -1,3 +1,18 @@
+<?php
+
+if ($_SERVER['REQUEST_URI'] == "/request-1/index.php") {
+  $home = "index.php";
+  $op = "pages/payments_register.php";
+  $rs = "pages/register_supplier.php";
+}else {
+  $home = "../index.php";
+  $op = "payments_register.php";
+  $rs = "register_supplier.php";
+}
+
+?>
+
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="#">Getpower</a>
@@ -6,20 +21,24 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <?php
+            echo '
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+              <a class="nav-link active" aria-current="page" href="'.$home.'">Home</a>
             </li>
+
             
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Formulario
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="pages/payments_register.php">Ordem Pagamento</a></li>
-                <li><a class="dropdown-item" href="pages/register_supplier.php">Cadastrar Fornecedor</a></li>
+                <li><a class="dropdown-item" href="'.$op.'">Ordem Pagamento</a></li>
+                <li><a class="dropdown-item" href="'.$rs.'">Cadastrar Fornecedor</a></li>
               </ul>
-            </li>
-
+            </li>'
+            ?>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="pages/releases_records.php">Listar Lançamentos</a>
             </li>
